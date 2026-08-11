8459c65c7a4d21689ab40471d09ad85be9a9a271
Sharan Pai <80072598+sharanpai3@users.noreply.github.com>
2026-08-12 00:42:43 +0530

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.go
@@ -0,0 +1,15 @@
+
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+
+	sRunes, tRunes := []rune(s), []rune(t)
+	sort.Slice(sRunes, func(i,j int)bool{
+		return sRunes[i]<sRunes[j]
+	})
+	sort.Slice(tRunes, func(i,j int)bool{
+		return tRunes[i]<tRunes[j]
+	})
+	return string(sRunes) == string(tRunes)
+}
